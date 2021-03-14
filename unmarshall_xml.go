7d07b827d03a39f6249f7ad2473343e435489afb
tyagian <tyagi.an@husky.neu.edu>
2021-03-14 00:43:05 -0500

    unmarshall xml

---
+++ unmarshall_xml.go
@@ -0,0 +1,91 @@
+package main
+import ("fmt"
+	"net/http"
+"io/ioutil"
+"encoding/xml")
+
+// index structure at the time of writing this code 
+/*
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/politics.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/opinions.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/local.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/sports.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/national.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/world.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/business.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/technology.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/lifestyle.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/entertainment.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/goingoutguide.xml
+</loc>
+</sitemap>
+</sitemapindex>
+
+*/
+
+type SitemapIndex struct {
+	// slice of type Location
+	Locations [] Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
+func main() {
+	resp,_ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes,_ := ioutil.ReadAll(resp.Body)
+	//string_body := string(bytes)
+	//fmt.Println(string_body)
+	resp.Body.Close()
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+	fmt.Println(s.Locations)
+}
