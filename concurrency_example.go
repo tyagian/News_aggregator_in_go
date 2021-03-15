6dc265a767be6dacdc0850bf53341b0834a31a60
tyagian <tyagi.an@husky.neu.edu>
2021-03-15 00:28:55 -0400

    defer

--- concurrency_example.go
+++ concurrency_example.go
@@ -8,11 +8,12 @@ import (
 var wg sync.WaitGroup
 
 func say(s string) {
+	defer wg.Done()
 	for i := 0; i <3; i++ {
-		fmt.Println(s)
 		time.Sleep(time.Millisecond*100)
+		fmt.Println(s)
 	}
-	wg.Done()
+	//wg.Done()
 }
 
 func main() {
